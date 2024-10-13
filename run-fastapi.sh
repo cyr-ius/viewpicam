a58cc61d374dff2daefbed5261d2ea621d48a780
cyr-ius <cyr-ius@ipocus.net>
2024-10-13 13:50:52 +0200

    Add run-fastapi wrapper for nginx

---
+++ run-fastapi.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -euo pipefail
+cd /app
+
+PATH=/env/bin:$PATH
+
+LOG_LEVEL="${LOG_LEVEL:-info}"
+
+
+if [ ! -f "./config/.secret_key" ]; then
+    secret=$(python -c 'import secrets; print(secrets.token_hex())')
+    echo SECRET_KEY=${secret} >> "./config/.secret_key"
+fi
+
+/bin/sh -c "alembic upgrade head"
+exec fastapi run app/main.py &
